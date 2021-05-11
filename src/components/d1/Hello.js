f0f1ed571ddada8bb70aa36f124ba6fa11d2e976
chimco <72544949+nsw1055@users.noreply.github.com>
2021-05-11 10:23:44 +0900

    day1

--- src/components/d1/Hello.js
+++ src/components/d1/Hello.js
@@ -3,7 +3,7 @@ import { Button, Typography } from "@material-ui/core";
 
 
 const Hello = ({name, fn}) => {
-
+    
     
     return ( <>
         <Typography component = "h1" variant = "h1"> Hello {name}</Typography>
